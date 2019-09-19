b33f9fecfa2aa567e00cb5045b79f0e99d93a493
qiuxk <qxk1101@163.com>
2019-09-19 18:01:16 +0800

    spring in action first commit

---
+++ src/main/java/com/qiuxk/entity/Student.java
@@ -0,0 +1,11 @@
+package com.qiuxk.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+    private String age;
+    private String sex;
+}
---
+++ src/main/java/com/qiuxk/mapping/StudentMapper.java
@@ -0,0 +1,10 @@
+package com.qiuxk.mapping;
+
+import com.qiuxk.entity.Student;
+
+import java.util.List;
+
+public interface StudentMapper {
+
+    public List<Student> queryAllStudent();
+}
